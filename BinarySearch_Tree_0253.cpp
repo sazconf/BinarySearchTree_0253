8f0d0d5b3c776413f194513c040a97004f183caf
sazconf <md.sazzad.ft24@mail.umy.ac.id>
2025-06-24 10:19:41 +0700

    Write the insert function

--- BinarySearch_Tree_0253.cpp
+++ BinarySearch_Tree_0253.cpp
@@ -21,4 +21,36 @@ class BinaryTree{
          BinaryTree(){
             ROOT= nullptr ;
          }
+
+         void insert (){
+            int x;
+            cout<<"Masukkan nilai: ";
+            cin >> x;
+            //Step1: Allocate memory for the new node
+            Node* newNode = new Node();
+            //Step2: Assign value to the data field of new node.
+            newNode ->info = x;
+
+            //Step3: Make the left and rigbt child of the new node point to  NULL;
+            newNode -> leftchild = nullptr;
+            newNode -> rightchild= nullptr;
+
+            //step4: Locate the node which will be the parent of the node to be inserted
+
+            Node *parent = nullptr;
+            Node *currentNode = nullptr;
+            search(x, parent, currentNode);
+
+            //Step5: If parent is NULL , tree is empty.
+            if (parent == nullptr){
+                //5a : Mark the new node as ROOT
+                ROOT = newNode;
+                //5b : Exit
+
+            }
+
+            }
+
+
+         }
 };
